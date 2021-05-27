4c1aebba5aadb33ea47e69feb8e3c92b38885c4b
Jimwa2 <65366040+Jimwa2@users.noreply.github.com>
2021-05-26 20:46:18 -0400

    Overhauled populate_board and generate_sublists. Created add, remove, and swap functions.

--- board.py
+++ board.py
@@ -7,6 +7,10 @@ class Board:
         self.row_list = {}
         self.column_list = {}
         self.three_by_three_list = {}
+        self.poss_choices_row = {}
+        self.poss_choices_column = {}
+        self.poss_choices_three_by_three = {}
+        self.failed_coordinates = []
 
 
     def __repr__(self):
@@ -55,45 +59,155 @@ class Board:
 
                 
     def generate_sublists(self):
-        #This function adds key:value pairs to the self.row_list dictionary.
-        #The keys are the numbers 1 through 9, and each key has a value that is a list of digits from 1 to 9.
-        #This row_list dictionary is then copied to the column and three by three dictionaries at the end.
-        #These dictionaries will be used by the populate_board() function to adhere to the rules of Sudoku.
+        #This function generates the lists used to adhere to the rules of Sudoku.
+
+        #Generates row, column, and three_by_three lists of coordinates.
+        for num in range(1, 10):
+            self.row_list[num] = []
+            self.column_list[num] = []
+            self.three_by_three_list[num] = []
+        for coordinate in self.board:
+            self.row_list[coordinate[0]].append(coordinate)
+            self.column_list[coordinate[1]].append(coordinate)
+            self.three_by_three_list[coordinate[2]].append(coordinate)
+
+        #Generates poss_choices lists of possible choices from 1 to 9.
         key_pointer = 1
         while key_pointer < 10:
-            self.row_list[key_pointer] = []
-            self.column_list[key_pointer] = []
-            self.three_by_three_list[key_pointer] = []
+            self.poss_choices_row[key_pointer] = []
+            self.poss_choices_column[key_pointer] = []
+            self.poss_choices_three_by_three[key_pointer] = []
             for i in range(1, 10):
-                self.row_list[key_pointer].append(i)
-                self.column_list[key_pointer].append(i)
-                self.three_by_three_list[key_pointer].append(i)
-            key_pointer += 1          
+                self.poss_choices_row[key_pointer].append(i)
+                self.poss_choices_column[key_pointer].append(i)
+                self.poss_choices_three_by_three[key_pointer].append(i)
+            key_pointer += 1
 
+
+    def add_choice_to_coordinate(self, coordinate, choice_to_be_added):
+        self.board[coordinate] = choice_to_be_added
+        self.poss_choices_row[coordinate[0]].remove(choice_to_be_added)
+        self.poss_choices_column[coordinate[1]].remove(choice_to_be_added)
+        self.poss_choices_three_by_three[coordinate[2]].remove(choice_to_be_added)
+        print(coordinate, "set to:", choice_to_be_added)
+
+
+    def remove_choice_from_coordinate(self, coordinate):
+        choice_to_be_removed = self.board[coordinate]
+        self.board[coordinate] = None
+        self.poss_choices_row[coordinate[0]].append(choice_to_be_removed)
+        self.poss_choices_column[coordinate[1]].append(choice_to_be_removed)
+        self.poss_choices_three_by_three[coordinate[2]].append(choice_to_be_removed)
+        print(coordinate, "set to: None")
+
+
+    def look_for_possible_swap(self, coordinate):
+        choice_to_be_added = 0
+        poss_choices_row = self.poss_choices_row[coordinate[0]]
+        poss_choices_column = self.poss_choices_column[coordinate[1]]
+        poss_choices_three_by_three = self.poss_choices_three_by_three[coordinate[2]]
+        
+        print("Looking for coordinate to swap with...")
+
+        #Search through other coordinates within the current row for a possible swap.
+        for other_coord in self.row_list[coordinate[0]]:
+            #For each choice within the current coordinate's poss_choices_row...
+            for choice in poss_choices_row:
+                #if the choice is in the other_coord's poss_choices_column and poss_choices_three_by_three...
+                if choice in self.poss_choices_column[other_coord[1]] and choice in self.poss_choices_three_by_three[other_coord[2]]:
+                    #and the other_coord's value is a valid choice for the current coordinate...
+                    if self.board[other_coord] in poss_choices_column and self.board[other_coord] in poss_choices_three_by_three:
+                        choice_to_be_added = self.board[other_coord]
+                        self.remove_choice_from_coordinate(other_coord)
+                        self.add_choice_to_coordinate(other_coord, choice)
+                        break
+        if choice_to_be_added:
+            self.add_choice_to_coordinate(coordinate, choice_to_be_added)
+            return
+
+        #Search through other coordinates within the current column for a possible swap.
+        for other_coord in self.column_list[coordinate[1]]:
+            ###
+            for choice in poss_choices_column:
+                ###
+                if choice in self.poss_choices_row[other_coord[0]] and choice in self.poss_choices_three_by_three[other_coord[2]]:
+                    ###
+                    if self.board[other_coord] in poss_choices_row and self.board[other_coord] in poss_choices_three_by_three:
+                        choice_to_be_added = self.board[other_coord]
+                        self.remove_choice_from_coordinate(other_coord)
+                        self.add_choice_to_coordinate(other_coord, choice)
+                        break
+        if choice_to_be_added:
+            self.add_choice_to_coordinate(coordinate, choice_to_be_added)
+            return
+
+        #Search through other coordinates within the current three_by_three for a possible swap.
+        for other_coord in self.three_by_three_list[coordinate[2]]:
+            ###
+            for choice in poss_choices_three_by_three:
+                ###
+                if choice in self.poss_choices_row[other_coord[0]] and choice in self.poss_choices_column[other_coord[1]]:
+                    ###
+                    if self.board[other_coord] in poss_choices_row and self.board[other_coord] in poss_choices_column:
+                        choice_to_be_added = self.board[other_coord]
+                        self.remove_choice_from_coordinate(other_coord)
+                        self.add_choice_to_coordinate(other_coord, choice)
+                        break
+        if choice_to_be_added:
+            self.add_choice_to_coordinate(coordinate, choice_to_be_added)
+            return
+
+        if not choice_to_be_added:
+            print("No valid choice found for coordinate:", coordinate)
+            self.failed_coordinates.append(coordinate)
+
+
+    def min_len_list_index(self, row_list, column_list, three_by_three_list):
+        combined_list = [row_list, column_list, three_by_three_list]
+        min_len_list = min(combined_list, key = len)
+        return combined_list.index(min_len_list)
 
     def populate_board(self):
         for coordinate in self.board:
-            print("Setting coordinate ", coordinate)
-            possible_value = 0
-            invalid_values = []
-            while True:
-                print("coordinate = ", coordinate)
-                possible_value = random.choice(self.row_list[coordinate[0]])
-                print("possible_value = " + str(possible_value))
-                print("invalid_values = " + str(invalid_values))
-                if possible_value not in invalid_values:
-                    if possible_value in self.column_list[coordinate[1]] and possible_value in self.three_by_three_list[coordinate[2]]:
-                        print("three_by_three_list = " + str(self.three_by_three_list[coordinate[2]]))
-                        print("row_list = " + str(self.row_list[coordinate[0]]))
-                        print("column_list = " + str(self.column_list[coordinate[1]]))
-                        self.board[coordinate] = possible_value
-                        self.row_list[coordinate[0]].remove(possible_value)
-                        self.column_list[coordinate[1]].remove(possible_value)
-                        self.three_by_three_list[coordinate[2]].remove(possible_value)
-                        break
-                    else:
-                        invalid_values.append(possible_value)
+            print("\nSetting coordinate ", coordinate)
+            choice_list = []
+            choice_to_be_added = 0
+            poss_choices_row = self.poss_choices_row[coordinate[0]]
+            poss_choices_column = self.poss_choices_column[coordinate[1]]
+            poss_choices_three_by_three = self.poss_choices_three_by_three[coordinate[2]]
+            print("poss_choices_row = " + str(poss_choices_row))
+            print("poss_choices_column = " + str(poss_choices_column))
+            print("poss_choices_three_by_three = " + str(poss_choices_three_by_three))
 
+            for choice in poss_choices_row:
+                if choice in poss_choices_column:
+                    if choice in poss_choices_three_by_three:
+                        choice_list.append(choice)
+            print("choice_list =", choice_list)
+
+            if choice_list:
+                choice_to_be_added = random.choice(choice_list)
+                self.add_choice_to_coordinate(coordinate, choice_to_be_added)
+
+            else:
+                self.look_for_possible_swap(coordinate)
+
+        print(self.poss_choices_row)
+        print(self.poss_choices_column)
+        print(self.poss_choices_three_by_three)
+
+        if self.failed_coordinates:
+            print("\nFixing failed coordinates:", self.failed_coordinates)
+            for failed_coordinate in self.failed_coordinates:
+                self.look_for_possible_swap(failed_coordinate)
+                self.failed_coordinates.remove(failed_coordinate)
+
+
+            
+
+
+
+            
                 
 
 
@@ -101,7 +215,6 @@ class Board:
 test_board = Board('hard')
 test_board.create_board()
 test_board.generate_sublists()
-#print(test_board.row_list)
 test_board.populate_board()
 
-print(test_board)
+print("\n", test_board)
